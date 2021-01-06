4935dcb0f59cac1ce3365be404fe97433e6d670c
Sally Perez <seattlesally1@gmail.com>
2021-01-05 17:03:37 -0700

    initial files and folders - db/schem and seeds, config - connection, installed npm dependencies - mysql, express, handlebars

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// burger app - connection.js
+
+const mysql = require("mysql");
+const util = require("util"); // used to promisify function
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootSal1",
+    database: "burgers_db"
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+});
+
+// uses promises instead of callbacks
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- burgers app seeds.sql
+USE burgers_db;
+
+insert into burgers (burger_name)
+values ('Impossible Cheeseburger'),
+('The Master Cheese'),
+('Royal Red Robin Burger'),
+('Bacon Cheeseburger');
